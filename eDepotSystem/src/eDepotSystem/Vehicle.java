4043fdecdbff26d02c52a226b48fcc41ec13fdf7
Curtis Myers <curtismyers25@gmail.com>
2020-04-14 10:22:35 +0100

    changes made to csv and classes not final at all so can change

--- eDepotSystem/src/eDepotSystem/Vehicle.java
+++ eDepotSystem/src/eDepotSystem/Vehicle.java
@@ -1,57 +1,142 @@
 package eDepotSystem;
 
+import java.util.List;
+
 public class Vehicle {
 	
 	//instance fields
-	
+	public String depotName; 	// Name of depot located at
+	public String location;  	// Actual Location
+	public int driverID;		// DriverID	
+	public int DepotID;
 	public String make;
 	public String model;
 	public int weight;
 	public String RegNo;
+	
 	public static boolean IsAvailable;
 	
-	protected List<Job> jobs = new ArrayList<Job>();
+	//protected List<Job> jobs = new ArrayList<Job>();
 	
-	public Vehicle (String make,String model, int weight, String RegNo) {
-		this.make = make;
-		this.model = model;
-		this.weight = weight;
-		this.RegNo = RegNo;
+	public Vehicle () {
+		
 	
 	}
 	
+	
+	
+	public String getDepotName() {
+		return depotName;
+	}
+
+
+
+	public void setDepotName(String depotName) {
+		this.depotName = depotName;
+	}
+
+
+
+	public String getLocation() {
+		return location;
+	}
+
+
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+
+
+	public int getDriverID() {
+		return driverID;
+	}
+
+
+
+	public void setDriverID(int driverID) {
+		this.driverID = driverID;
+	}
+
+
+
+	public int getDepotID() {
+		return DepotID;
+	}
+
+
+
+	public void setDepotID(int depotID) {
+		DepotID = depotID;
+	}
+
+
+
 	public String getMake() {
 		return make;
-		
 	}
-	
+
+
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+
+
 	public String getModel() {
 		return model;
-		
 	}
-	
+
+
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+
+
 	public int getWeight() {
 		return weight;
-		
 	}
-	
+
+
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+
+
 	public String getRegNo() {
 		return RegNo;
-		
 	}
+
+
+
+	public void setRegNo(String regNo) {
+		RegNo = regNo;
+	}
+
+
+
+	public String toString() {
+        return  " Make:" +this.depotName + " Tanker Model:" + this.location + " Tanker RegNo:" + this.driverID + " DepotID: " + this.DepotID + " Make: " + this.make + " Model: " + this.model + " Weight: " + this.weight + " RegNO: " + this.RegNo;
+    }
 	
-	public void makeJob(Job jobDateTime) {
-		jobs.add(jobDateTime);
+	//public void makeJob(Job jobDateTime) {
+	//	jobs.add(jobDateTime);
 		
-	}
+	//}
 	
-	public Job getNextJob() {
+	//public Job getNextJob() {
 		
-		jobs.sort(Comparator.comparing(o -> o.getStartDate()));
+	//	jobs.sort(Comparator.comparing(o -> o.getStartDate()));
 		
-		return jobs.stream().filter(o -> o.getStartDate().isAfter(LocalDateTime.now())).findFirst().get();
+	//	return jobs.stream().filter(o -> o.getStartDate().isAfter(LocalDateTime.now())).findFirst().get();
 		
-	}
+	//}
 
 	
 
