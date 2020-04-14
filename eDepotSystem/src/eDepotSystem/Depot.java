5be2b580a07a9b0a44d5508fe9f68ba550aabb9d
Caolan <Caolan@LAPTOP-85EIMITV>
2020-04-15 00:10:57 +0100

    Methods Added and menu upgrade

--- eDepotSystem/src/eDepotSystem/Depot.java
+++ eDepotSystem/src/eDepotSystem/Depot.java
@@ -1,7 +1,68 @@
 package eDepotSystem;
 
+import java.util.ArrayList;
+
 public class Depot {
 
-	
+	private String depot;
+	private List<Job> jobs = new ArrayList<Job>();
+	private List<Driver> drivers = new ArrayList<Driver>();
+	private List<Vehicle> vehicles = new ArrayList<Vehicle>();
 
+	
+	
+	
+	public Depot(String depot) {
+		this.depot = depot;
+		
+	
+	}
+	
+	public void addDriver(Driver driver) {
+		//validate?
+		
+		drivers.add(driver);
+			
+	}
+	
+	public Driver getDriverByName(String name) {
+		
+		for (Driver d : drivers) {
+			if (d.getName().equals(name)){
+				return d;
+				
+			}
+			}
+		//if no match
+		return null;
+		
+		public void addVehicle(Vehicle vehicle) {
+			//validate?
+			
+			vehicles.add(vehicle);
+				
+		}
+		
+		public Vehicle getVehicleByRegNo(String regno) {
+			
+			for (Vehicle v : vehicles) {
+				if (v.getName().equals(regno)){
+					return d;
+					
+				}
+				}
+			//if no match
+			return null;
+	}
+		
+		public void makeJob(Job job) {
+			jobs.add(job);
+			job.getDriver().makeJob(job);
+			job.getVehicle().makeJob(job);
+		
+		}
+
+		public void makeJob(String ref, Driver drivers, Vehicle vehicles, LocalDateTime jobStartDate) {
+			
+		}
 }
--- eDepotSystem/src/eDepotSystem/eDepotSystem.java
+++ eDepotSystem/src/eDepotSystem/eDepotSystem.java
@@ -5,12 +5,14 @@ import java.util.Scanner;
 public class eDepotSystem {
 	static Scanner scan = new Scanner(System.in);
 	static int inputNumber;
+	private List<Depot> depots = new ArrayList<Depot>();
+	private Depot depot;
 	
-
 	public static void main(String[] args) {
 		
 		displayMenu();
 
+		
 	}
 	
 	private static void displayMenu() {
@@ -19,8 +21,12 @@ public class eDepotSystem {
 			System.out.println("Welcome to the eDepotSystem\n");
 			System.out.println("Below you will find the menu which shows which services are available on the system\n");
 			System.out.println("Menu:");
-			System.out.println("(1) View Depots on the system");
-			
+			System.out.println("(1) View Depots by name");
+			System.out.println("(2) View Jobs by Ref No.");
+			System.out.println("(3) View Drivers by ID");
+			System.out.println("(4) View Vehicles by Reg No.");
+			System.out.println("(5) Create a new Job");
+			System.out.println("(6) View Depots next job ");
 			String userChoice =scan.nextLine();
 			
 			try {
@@ -31,14 +37,88 @@ public class eDepotSystem {
 			
 			switch(inputNumber) {
 				case 1: 
-					displayMenu();
+					getDepotByName();
 					break;
+				case 2: 
+					getJobRef();
+					break;
+				case 3: 
+					getDriverByID();
+					break;
+				case 4: 
+					getVehicleByReg();
+					break;
+				case 5: 
+					makeJob();
+					break;
+				case 6: 
+					getNextJob();
+					break;
+			}	
 			}
-			
-			
+		
 		}
+	
+	private void getDepotByName() {
+		System.out.println("Enter Depots Name : ");
+		String depotName = scan.nextLine();
+		
+		System.out.printl n(depots.getDepotByName(depotName).toString());
+	}
+	
+	private void getJobRef() {
+		System.out.println("Enter Job Ref number : ");
+		String jobRef = scan.nextLine();
+		
+		System.out.println(depots.getJobRef(jobRef).toString());
+	}
+	private void getDriverByID() {
+		System.out.println("Enter Drivers ID : ");
+		String driverID = scan.nextLine();
+		
+		System.out.println(depots.getDriverByID(driverID).toString());
+	}
+	private void getVehicleByReg() {
+		System.out.println("Enter Vehicle Reg : ");
+		String vehicleReg = scan.nextLine();
+		
+		System.out.println(depots.getVehicleByReg(vehicleReg).toString());
+	}
+	
+	
+	private void makeJob() {
+		System.out.println("Enter the new Job Ref No. : ");
+		String ref = scan.nextLine();
+		
+		System.out.println("Enter the Depots Name : ");
+		String depotName = scan.nextLine();
+		
+		System.out.println("Enter the Driver ID : ");
+		String driverID = scan.nextLine();
+		
+		System.out.println("Enter the Vehicle RegNo.: ");
+		String vehicleReg = scan.nextLine();
+		
+		Job jobs = depots.getJobRef(ref);
+		Depot depots = depots.getDepotByName(depotName);
+		Driver drivers = depots.getDriverByID(driverID);
+		Vehicle vehicles = depots.getVehicleByReg(vehicleReg);
+		
+		System.out.println("Enter the Job's creation date (d mm yy HH:mm:");
+		LocalDateTime jobStartDate = LocalDateTime.parse(scan.nextLine(), DateTimeFormatter.ofPattern("dd MM yy HH:mm"));
+		
+		depots.makeJob(new Job(ref, depotName, driverID, vehicleReg, jobStartDate));
+		
+	
+	}
+	private void getNextJob() {
+		System.out.println("Enter the Depot Name to see the next booking : ");
+		String depotName = scan.nextLine();
+	 
+		
+		System.out.println(depots.getDepotByName(depotName).getNextJob().getJobDateTime().format(DateTimeFormatter.ofPattern("dd MM yy HH:mm")));)
+		
 		
 	}
 	
-
 }
